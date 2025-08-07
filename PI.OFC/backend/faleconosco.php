c584fa2109f534db40c6432db810a41c135d5b09
Ribeirozjki <phassumpcao246@gmail.com>
2025-08-07 09:03:27 -0300

    Update faleconosco.php

--- PI.OFC/backend/faleconosco.php
+++ PI.OFC/backend/faleconosco.php
@@ -1,17 +1,29 @@
 <?php
-include_once("conexao.php");
-$nome_completo = $_POST['nome_completo'];
+$servidor = "localhost";
+$usuario = "root";
+$senha = "123456";
+$banco = "AquaLimpa"; 
+
+$conn = mysqli_connect($servidor, $usuario, $senha, $banco);
+
+if (!$conn) {
+    die("Falha na conexão: " . mysqli_connect_error());
+}
+
+$nome_completo = $_POST['nome'];
 $email = $_POST['email'];
 $telefone = $_POST['telefone'];
 $mensagem = $_POST['mensagem'];
 
-$sql = "INSERT INTO mensagens(nome_completo, email,telefone,mensagem )
+$sql = "INSERT INTO mensagens(nome_completo, email, telefone, mensagem)
 VALUES ('$nome_completo', '$email', '$telefone', '$mensagem')";
-if (mysqli_query($conexao, $sql))
-{
-    header("Location:index.html");
+
+if (mysqli_query($conn, $sql)) {
+    header("Location: ../index.html"); 
+    exit;
 } else {  
-    echo "Erro ao cadastrar: " . mysqli_error($conexao);
+    echo "Erro ao cadastrar: " . mysqli_error($conn);
 }
-mysqli_close($conexao);
+
+mysqli_close($conn);
 ?>
